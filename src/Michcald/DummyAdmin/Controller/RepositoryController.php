2b7bcd8cd561f22c0a0c21ac2116f306cfe84758
michael <michael@michaelcloud>
2014-06-08 13:28:37 +0100

    Improvements

--- src/Michcald/DummyAdmin/Controller/RepositoryController.php
+++ src/Michcald/DummyAdmin/Controller/RepositoryController.php
@@ -113,13 +113,62 @@ class RepositoryController extends \Michcald\Mvc\Controller\HttpController
         $json = $resp->getContent();
         $entity = json_decode($json, true);
         
-        //\Zend\Debug\Debug::dump($entity);die;
+        $errors = null;
+        
+        if ($this->getRequest()->isMethod('POST')) {
+            
+            $data = $this->getRequest()->getData();
+            
+            $dataFiltered = $data;
+            
+            // file managing
+            foreach ($data as $key => $value) {
+                if (isset($data[$key]['tmp_name'])) {
+                    if ($data[$key]['tmp_name'] != '') {
+                        $filename = 'tmp/' . $data[$key]['name'];
+
+                        move_uploaded_file(
+                            $data[$key]['tmp_name'], 
+                            $filename
+                        );
+
+                        $dataFiltered[$key] = '@' . $filename;
+                    } else {
+                        $dataFiltered[$key] = null;
+                    }
+                }
+            }
+            
+            $resp = $app->call('put', $repository . '/' . $id, $dataFiltered);
+            $statusCode = $resp->getStatusCode();
+            $json = $resp->getContent();
+            $respJson = json_decode($json, true);
+            
+            //\Zend\Debug\Debug::dump($respJson);die;
+            
+            if ($resp->getStatusCode() == 400) {
+                $errors = $respJson['error'];
+            } else if ($resp->getStatusCode() == 200) {
+                $success = true;
+            } else {
+                $success = false;
+            }
+            
+            $this->emptyTmpDir();
+            
+        } else {
+            $dataFiltered = $entity;
+        }
+        
+        $dataFiltered['id'] = $id;
         
         $page = $this->getView()->render(
             '../app/views/repository/edit.phtml',
             array(
                 'repository' => $repoInfo,
-                'entity'     => $entity
+                'entity'     => $dataFiltered,
+                'error'      => $errors,
+                'success'    => isset($success) ? true : false
             )
         );
         
@@ -139,78 +188,149 @@ class RepositoryController extends \Michcald\Mvc\Controller\HttpController
     
     public function deleteAction($repository, $id)
     {
-        $client = new \Michcald\RestClient\Client();
+        $app = \Michcald\Mvc\Container::get('dummy.app');
         
-        $auth = new \Michcald\RestClient\Auth\Basic();
-        $auth->setUsername('stefano')
-                ->setPassword('123456');
-        $client->setAuth($auth);
-        
-        $resp = $client->delete($this->baseUrl . "/{$repository}/{$id}");
+        $resp = $app->call('get', $repository . '/_info');
         $statusCode = $resp->getStatusCode();
         $json = $resp->getContent();
-        $info = json_decode($json, true);
+        $repoInfo = json_decode($json, true);
         
-        return $this->listAction($repository);
-    }
-    
-    public function createAction($repository)
-    {
-        $client = new \Michcald\RestClient\Client();
-        
-        $auth = new \Michcald\RestClient\Auth\Basic();
-        $auth->setUsername('stefano')
-                ->setPassword('123456');
-        $client->setAuth($auth);
-        
-        $resp = $client->get($this->baseUrl . "/{$repository}/_info");
+        $resp = $app->call('get', $repository . '/' . $id);
         $statusCode = $resp->getStatusCode();
         $json = $resp->getContent();
-        $info = json_decode($json, true);
+        $entity = json_decode($json, true);
         
         if ($this->getRequest()->isMethod('POST')) {
-            
-            $data = $this->getRequest()->getData();
-            
-            // TODO for every file
-            $filename = 'tmp/' . $data['img']['name'];
-            
-            move_uploaded_file(
-                $data['img']['tmp_name'], 
-                $filename
-            );
-            
-            $data['img'] = '@' . $filename;
-            
-            $resp = $client->post(
-                $this->baseUrl . "/{$repository}",
-                $data
-            );
-            
-            if ($resp->getStatusCode() != 200) {
-                var_dump(json_decode($resp->getContent(), true));
-                die;
+            $resp = $app->call('delete', $repository . '/' . $id);
+            $statusCode = $resp->getStatusCode();
+            if ($statusCode == 204) {
+                return $this->listAction($repository);
             }
-            
-            unlink($filename);
-            
-            var_dump(json_decode($resp->getContent(), true));
-            die('ok');
-            
-        } else {
-            $body = $this->getView()->render(
-                'views/repository/create.phtml',
-                array(
-                    'info' => $info
-                )
-            );
         }
-        //\Zend\Debug\Debug::dump($info);die;
+        
+        $page = $this->getView()->render(
+            '../app/views/repository/delete.phtml',
+            array(
+                'repository' => $repoInfo,
+                'entity'     => $entity,
+                'success'    => isset($success) ? $success : null
+            )
+        );
+        
+        $body = $this->getView()->render(
+            '../app/views/layout.phtml',
+            array(
+                'page' => $page
+            )
+        );
         
         $response = new \Michcald\Mvc\Response();
         $response->setContent('text/html')
                 ->setContent($body);
         
         return $response;
+    }
+    
+    public function createAction($repository)
+    {
+        $app = \Michcald\Mvc\Container::get('dummy.app');
+        
+        $resp = $app->call('get', $repository . '/_info');
+        $statusCode = $resp->getStatusCode();
+        $json = $resp->getContent();
+        $repoInfo = json_decode($json, true);
+        
+        $errors = null;
+        
+        if ($this->getRequest()->isMethod('POST')) {
+            
+            $data = $this->getRequest()->getData();
+            
+            $dataFiltered = $data;
+            
+            // file managing
+            foreach ($data as $key => $value) {
+                if (isset($data[$key]['tmp_name'])) {
+                    if ($data[$key]['tmp_name'] != '') {
+                        $filename = 'tmp/' . $data[$key]['name'];
+
+                        move_uploaded_file(
+                            $data[$key]['tmp_name'], 
+                            $filename
+                        );
+
+                        $dataFiltered[$key] = '@' . $filename;
+                    } else {
+                        $dataFiltered[$key] = null;
+                    }
+                }
+            }
+            
+            $resp = $app->call('post', $repository, $dataFiltered);
+            $statusCode = $resp->getStatusCode();
+            $json = $resp->getContent();
+            $respJson = json_decode($json, true);
+            
+            //\Zend\Debug\Debug::dump($respJson);die;
+            
+            if ($resp->getStatusCode() == 400) {
+                $errors = $respJson['error'];
+            } else if ($resp->getStatusCode() == 200) {
+                $success = true;
+            } else {
+                $success = false;
+            }
+            
+            $this->emptyTmpDir();
+            
+        } else {
+            $dataFiltered = array();
+            foreach ($repoInfo['fields'] as $field) {
+                if ($field['name'] == 'id') {
+                    continue;
+                }
+                $dataFiltered[$field['name']] = null;
+            }
+        }
+        
+        $page = $this->getView()->render(
+            '../app/views/repository/create.phtml',
+            array(
+                'repository' => $repoInfo,
+                'entity'     => $dataFiltered,
+                'error'      => $errors,
+                'success'    => isset($success) ? true : false
+            )
+        );
+        
+        $body = $this->getView()->render(
+            '../app/views/layout.phtml',
+            array(
+                'page' => $page
+            )
+        );
+        
+        $response = new \Michcald\Mvc\Response();
+        $response->setContent('text/html')
+                ->setContent($body);
+        
+        return $response;
+    }
+    
+    private function emptyTmpDir()
+    {
+        $dir = 'tmp';
+        
+        $iter = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS),
+            \RecursiveIteratorIterator::SELF_FIRST,
+            \RecursiveIteratorIterator::CATCH_GET_CHILD // Ignore "Permission denied"
+        );
+
+        foreach ($iter as $path => $d) {
+            if ($d->isFile()) { // isDir()
+                unlink($d);
+            }
+        }
     }
 }
